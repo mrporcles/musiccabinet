96208c5b24909bba5fd90a5bc4f322df2cdc48ec
mrporcles <mrporcles@users.noreply.github.com>
2014-09-10 11:38:51 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,11 +5,6 @@ set -e
 [ ! -L /data/transcode ] && ln -s /var/subsonic.default/transcode /data/transcode
 
 sed '/-Dmusiccabinet.log.fileName/ a\ -Dmusiccabinet.jdbc.url=jdbc:postgresql://'$DB_PORT_5432_TCP_ADDR':5432/musiccabinet \\' /usr/share/subsonic/subsonic.sh > /usr/share/subsonic/subsonictmp.sh 
-sed '/-Dmusiccabinet.log.fileName/ a\ -Dmusiccabinet.jdbc.initialurl=jdbc:postgresql://'$DB_PORT_5432_TCP_ADDR':5432/template1 \\' /usr/share/subsonic/subsonictmp.sh > /usr/share/subsonic/subsonic.sh
+sed '/-Dmusiccabinet.jdbc.url/ a\ -Dmusiccabinet.jdbc.initialurl=jdbc:postgresql://'$DB_PORT_5432_TCP_ADDR':5432/template1 \\' /usr/share/subsonic/subsonictmp.sh > /usr/share/subsonic/subsonicmc.sh
 
-rm -f /usr/share/subsonic/subsonictmp.sh
-
-echo $DB_PORT_5432_TCP_ADDR:5432
-cat /usr/share/subsonic/subsonic.sh
-
-/usr/share/subsonic/subsonic.sh
+/usr/share/subsonic/subsonicmc.sh
