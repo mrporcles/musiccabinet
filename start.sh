e3e484576a060b1ea4a1350e164393e689854a57
mrporcles <mrporcles@users.noreply.github.com>
2014-09-10 13:38:19 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ set -e
 sed '/-Dmusiccabinet.log.fileName/ a\ -Dmusiccabinet.jdbc.url=jdbc:postgresql://'$DB_PORT_5432_TCP_ADDR':5432/musiccabinet \\' /usr/share/subsonic/subsonic.sh > /usr/share/subsonic/subsonictmp.sh 
 sed '/-Dmusiccabinet.jdbc.url/ a\ -Dmusiccabinet.jdbc.initialurl=jdbc:postgresql://'$DB_PORT_5432_TCP_ADDR':5432/template1 \\' /usr/share/subsonic/subsonictmp.sh > /usr/share/subsonic/subsonicmc.sh
 
-echo $DB_PORT_%$£@_TCP_ADDR<------------------HERE
+echo $DB_PORT_5432_TCP_ADDR:5432
 cat /usr/share/subsonic/subsonicmc.sh
 
 
