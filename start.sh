4d7edd85bf9dca49067e8497002830c19a693c60
mrporcles <mrporcles@users.noreply.github.com>
2014-09-14 13:50:16 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,8 +4,8 @@ set -e
 
 [ ! -L /data/transcode ] && ln -s /var/subsonic.default/transcode /data/transcode
 
-sed '/-Dmusiccabinet.log.fileName/ a\ -Dmusiccabinet.jdbc.url=jdbc:postgresql://192.168.0.13:5432/musiccabinet \\' /usr/share/subsonic/subsonic.sh > /usr/share/subsonic/subsonictmp.sh 
-sed '/-Dmusiccabinet.jdbc.url/ a\ -Dmusiccabinet.jdbc.initialurl=jdbc:postgresql://192.168.0.13:5432/template1 \\' /usr/share/subsonic/subsonictmp.sh > /usr/share/subsonic/subsonicmc.sh
+sed '/-Dmusiccabinet.log.fileName/ a\ -Dmusiccabinet.jdbc.url=jdbc:postgresql://localhost:4242/musiccabinet \\' /usr/share/subsonic/subsonic.sh > /usr/share/subsonic/subsonictmp.sh 
+sed '/-Dmusiccabinet.jdbc.url/ a\ -Dmusiccabinet.jdbc.initialurl=jdbc:postgresql://localhost:4242/template1 \\' /usr/share/subsonic/subsonictmp.sh > /usr/share/subsonic/subsonicmc.sh
 
 chmod +x /usr/share/subsonic/subsonicmc.sh
 
